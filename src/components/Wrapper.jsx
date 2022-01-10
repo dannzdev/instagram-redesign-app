c139b6f475a9477d256e7e14e2cd229ca833b143
Daniel Sanchez <danielsanchez1502@gmail.com>
2022-01-10 17:07:34 -0300

    Response Config

--- src/components/Wrapper.jsx
+++ src/components/Wrapper.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 const Wrapper = styled.div`
 	width:  32rem;
 	height: 100vh;
-	margin: 1rem auto 1rem auto;
+	/* margin: 1rem auto 1rem auto; */
 	border-top-left-radius: 1.5rem;
 	border-top-right-radius: 1.5rem;
 	overflow: hidden;
